77ba275ebb35ef029453e0b113d1f6b51e63b730
Egor-Kozlov <kozlov1998.2010@gmail.com>
2024-01-30 11:06:14 +0100

    Add deploy workflow and set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Rick-and-Morty/",
   resolve: {
     alias: {
       "@app-hooks": path.resolve(__dirname, "./src/hooks"),
